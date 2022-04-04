7dba20009434197169186115675aab8c35ec58f8
Yuichi-Kadota <yuichi_kadota@mediado.jp>
2022-04-04 13:06:50 +0900

    ginの追加

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"trial-go-stacktrace/services/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+	engine.GET("/user", user.GetHandler())
+	engine.Run(":3000")
+}
